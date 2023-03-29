c231eb8a8744ee0d14008636dad1dfb8128526e9
Jonatan Lizarazu <jonilizarazu23@gmail.com>
2023-03-29 11:14:27 -0300

    JavaIntro01

---
+++ Ejercicio12.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaintroguia1;
+
+import java.util.Scanner;
+
+
+public class Ejercicio12 {
+    
+    public static void main(String[] args) {
+        Scanner leer = new Scanner(System.in);
+        
+            
+      
+    }
+    
+}
